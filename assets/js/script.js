b926a805c11038eec8b6d11de283906e746c8149
Phillip Baumgärtner <baumgaertner@haeuslerundbolay.de>
2016-11-09 15:22:35 +0100

    CodeKit Build Process hinzugefügt

--- assets/js/script.js
+++
@@ -1,2 +0,0 @@
-// @codekit-prepend "../../bower_components/jquery/dist/jquery.min.js";
-// @codekit-prepend "../../bower_components/bootstrap/dist/js/bootstrap.min.js";
---
+++ source/assets/js/script.js
@@ -0,0 +1,2 @@
+// @codekit-prepend "../../../bower_components/jquery/dist/jquery.min.js";
+// @codekit-prepend "../../../bower_components/bootstrap/dist/js/bootstrap.min.js";
